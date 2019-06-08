638a6525fabe89567089921fc23bdc96232446ec
Artem Zinnatullin :slowpoke <ceo@artemzin.com>
2019-06-08 14:58:36 -0700

    Add readme, refactor args (#2)

--- bintray-backuper/src/main/kotlin/io/buildfoundation/bintraybackuper/Args.kt
+++ bintray-backuper/src/main/kotlin/io/buildfoundation/bintraybackuper/Args.kt
@@ -10,6 +10,8 @@ import java.time.Duration
 
 data class Args(
     val subject: String,
+    val downloadDir: File,
+    val basicAuthCredentials: String?,
     val httpConnectionTimeout: Duration,
     val httpWriteTimeout: Duration,
     val httpReadTimeout: Duration,
@@ -18,9 +20,7 @@ data class Args(
     val httpThreads: Int,
     val checksumThreads: Int,
     val checksumBufferBytes: Int,
-    val downloadDir: File,
     val downloadRetries: Long,
-    val basicAuthCredentials: String?,
     val apiEndpoint: HttpUrl,
     val downloadsEndpoint: HttpUrl
 )
@@ -29,16 +29,19 @@ private class IntermediateArgs(parser: ArgParser) {
     val subject by parser
         .storing("--subject", help = "Bintray \"subject\", either org or user name that hosts files.")
 
+    val downloadDir by parser
+        .storing("--download-dir", help = "Directory to download files to. Files will be stored in following layout: 'download-dir/subject/repo/path-to-file'. Directory layout will be created if it doesn't exist. Checksum of existing files will be verified against metadata downloaded from Bintray.")
+
     val httpConnectionTimeoutSeconds by parser
         .storing("--http-connection-timeout", help = "HTTP connection timeout (seconds)")
         .default("30")
 
     val httpWriteTimeout by parser
-        .storing("--http-write-timeout", help = "HTTP write timeout (seconds)")
+        .storing("--http-write-timeout", help = "HTTP write timeout for individual socket write (seconds)")
         .default("60")
 
     val httpReadTimeout by parser
-        .storing("--http-read-timeout", help = "HTTP read timeout (seconds)")
+        .storing("--http-read-timeout", help = "HTTP read timeout for individual socket read (seconds)")
         .default("60")
 
     val httpCallTimeout by parser
@@ -61,21 +64,9 @@ private class IntermediateArgs(parser: ArgParser) {
         .storing("--checksum-buffer-bytes", help = "Checksum disk stream buffer (bytes)")
         .default("${16 * 1024}")
 
-    val downloadDir by parser
-        .storing("--download-dir", help = "Directory to download files to. Files will be stored in following layout: 'download-dir/subject/repo/path-to-file'. Directory layout will be created if it doesn't exist.")
-
     val downloadRetries by parser
         .storing("--download-retries", help = "Number of retries to attempt for each download")
         .default("3")
-
-    val credentials by parser
-        .storing("--credentials", help = "Basic auth credentials for Bintray in following format: 'user:apikey'")
-        .default("")
-        .addValidator {
-            if (value != "" && value.split(":").size != 2) {
-                throw SystemExitException("Credentials must be in 'user:apikey' format.", returnCode = 1)
-            }
-        }
 
     val apiEndpoint by parser
         .storing("--api-endpoint", help = "Bintray-compatible API endpoint to use")
@@ -109,6 +100,22 @@ fun parseArgs(rawArgs: Array<String>): Args {
 
         return Args(
             subject = intermediateArgs.subject,
+            basicAuthCredentials = System.getenv("BINTRAY_BACKUPER_API_CREDENTIALS") ?: "".let {
+                if (it == "") {
+                    null
+                } else {
+                    val split = it.split(":")
+
+                    if (split.size != 2) {
+                        throw SystemExitException("Credentials must be in 'user:apikey' format.", returnCode = 1)
+                    }
+
+                    val user = split[0]
+                    val apiKey = split[1]
+
+                    Credentials.basic(user, apiKey)
+                }
+            },
             httpConnectionTimeout = Duration.ofSeconds(intermediateArgs.httpConnectionTimeoutSeconds.toLong()),
             httpWriteTimeout = Duration.ofSeconds(intermediateArgs.httpWriteTimeout.toLong()),
             httpReadTimeout = Duration.ofSeconds(intermediateArgs.httpReadTimeout.toLong()),
@@ -119,18 +126,6 @@ fun parseArgs(rawArgs: Array<String>): Args {
             checksumBufferBytes = intermediateArgs.checksumBufferBytes.toInt(),
             downloadDir = File(intermediateArgs.downloadDir),
             downloadRetries = intermediateArgs.downloadRetries.toLong(),
-            basicAuthCredentials = intermediateArgs.credentials.let {
-                if (it == "") {
-                    null
-                } else {
-                    val split = it.split(":")
-
-                    val user = split[0]
-                    val apiKey = split[1]
-
-                    Credentials.basic(user, apiKey)
-                }
-            },
             apiEndpoint = intermediateArgs.apiEndpoint.let { HttpUrl.get(it) },
             downloadsEndpoint = intermediateArgs.downloadsEndpoint.let { HttpUrl.get(it) }
         )
